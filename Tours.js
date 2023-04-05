663834ef054e2520c70fde3d2964187b62c82eca
singla-kunal <101396855+singla-kunal@users.noreply.github.com>
2023-04-05 22:18:05 +0530

    Add files via upload

---
+++ Tours.js
@@ -0,0 +1,19 @@
+import Card from './Card';
+
+function Tours({tours, removeTour}) {
+    return (
+        <div>
+            <div>
+                <h2>Plan With Love</h2>
+            </div>
+            <div>
+                {
+                    tours.map((tour)=>{
+                        return <Card{...tour} removeTour={removeTour}></Card>
+                    })
+                }
+            </div>
+        </div>
+    );
+}
+export default Tours;
